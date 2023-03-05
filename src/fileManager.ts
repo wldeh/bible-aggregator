c24a3f4096950b4ca3f4861ab0be8e402162d958
ucig <ewthegreat4@gmail.com>
2023-03-04 23:47:17 -0800

    ✨ feat: add functionality to import a folder from a remote URL
    This commit adds a new public method called `importFolder` to the fileManager class, which allows users to download and extract files from a remote URL into a specified directory. The method uses the `getDownloadLink`, `download`, and `unzip` methods to perform these operations, and deletes the temporary downloaded file once the extraction is completed. Any errors that occur during the process are also handled and logged appropriately.

--- src/fileManager.ts
+++ src/fileManager.ts
@@ -1,54 +1,72 @@
-import * as cheerio from 'cheerio';
-import * as unzipper from 'unzip-stream';
-import fs from 'fs';
-import fetch, { Response } from 'node-fetch';
+import * as cheerio from 'cheerio'
+import * as unzipper from 'unzip-stream'
+import fs from 'fs'
+import fetch, { Response } from 'node-fetch'
 
 export default class fileManager {
-  static async getDownloadLink(url: string): Promise<string> {
+  protected static async getDownloadLink(url: string): Promise<string> {
     try {
-      const response: Response = await fetch(url);
-      const data: string = await response.text();
-      const $ = cheerio.load(data);
+      const response: Response = await fetch(url)
+      const data: string = await response.text()
+      const $ = cheerio.load(data)
       const href: string =
         'https://app.thedigitalbiblelibrary.org' +
-        $('#download_button').attr('href');
-      return href;
+        $('#download_button').attr('href')
+      return href
     } catch (error) {
-      console.error(error);
-      throw error;
+      console.error(error)
+      throw error
     }
   }
 
-  static async download(url: string): Promise<void> {
+  protected static async download(
+    url: string,
+    downloadPath: string
+  ): Promise<void> {
     try {
-      const response: Response = await fetch(url);
-      const blob: Blob = await response.blob();
-      const buffer = Buffer.from(await blob.arrayBuffer());
+      const response: Response = await fetch(url)
+      const blob: Blob = await response.blob()
+      const buffer = Buffer.from(await blob.arrayBuffer())
       fs.writeFile(
-        'file.zip',
+        downloadPath,
         buffer,
         (err: NodeJS.ErrnoException | null) => {
           if (err) {
-            console.error(err);
+            console.error(err)
           } else {
-            console.log('File downloaded successfully');
+            console.log('File downloaded successfully')
           }
         }
-      );
+      )
     } catch (error) {
-      console.error(error);
+      console.error(error)
     }
   }
 
-  static async unzip(path: string, outPath: string): Promise<void> {
+  protected static async unzip(
+    outPath: string,
+    downloadPath: string
+  ): Promise<void> {
     try {
       return fs
-        .createReadStream(path)
-        .pipe(unzipper.Extract({ path: outPath }));
+        .createReadStream(downloadPath)
+        .pipe(unzipper.Extract({ path: outPath }))
     } catch (error) {
-      console.error(error);
+      console.error(error)
+    }
+  }
+
+  public static async importFolder(url: string, outPath: string) {
+    try {
+      const downloadPath = `/tmp/${Math.random().toString(36).substring(2)}.zip`
+      const downloadLink: string = await this.getDownloadLink(url)
+      await this.download(downloadLink, downloadPath)
+      await this.unzip(outPath, downloadPath)
+      // Delete the temporary file
+      fs.unlinkSync(downloadPath)
+    } catch (error) {
+      // Handle errors here
+      console.error(error)
     }
   }
 }
-
-
