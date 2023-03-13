c307ca8a0684854ab893fc29d5736e3f3729ae84
ucig <ewthegreat4@gmail.com>
2023-03-13 01:37:41 -0700

    ✅ test: Added unit tests for unzip function
    Added two test cases to verify that the unzip function works correctly. The first case tests whether the file is unzipped successfully, while the second case tests whether the function throws an error if the unzip fails. Also mocked fs and unzip-stream modules to isolate the tests from other dependencies.

---
+++ tests/data/unzip/unzipper.test.ts
@@ -0,0 +1,56 @@
+import * as unzipFile from 'unzip-stream'
+import fs from 'fs'
+
+import unzip from '../../../src/data/unzip/unzipper'
+
+jest.mock('fs')
+jest.mock('unzip-stream')
+
+describe('unzip', () => {
+  it('should unzip the file', async () => {
+    const mockReadStream = {
+      pipe: jest.fn().mockReturnThis(),
+      on: jest.fn().mockImplementation((event, callback) => {
+        if (event === 'finish') {
+          callback()
+        }
+        return mockReadStream
+      })
+    }
+    ;(fs.createReadStream as jest.Mock).mockReturnValue(mockReadStream)
+
+    const outPath = '/out'
+    const downloadPath = '/download'
+    await unzip(outPath, downloadPath)
+
+    expect(fs.createReadStream).toHaveBeenCalledWith(downloadPath)
+    expect(unzipFile.Extract).toHaveBeenCalledWith({ path: outPath })
+    expect(mockReadStream.on).toHaveBeenCalledWith(
+      'error',
+      expect.any(Function)
+    )
+    expect(mockReadStream.on).toHaveBeenCalledWith(
+      'finish',
+      expect.any(Function)
+    )
+  })
+
+  it('should throw an error if unzip fails', async () => {
+    const mockReadStream = {
+      pipe: jest.fn().mockReturnThis(),
+      on: jest.fn().mockImplementation((event, callback) => {
+        if (event === 'error') {
+          callback(new Error('Unzip failed'))
+        }
+        return mockReadStream
+      })
+    }
+    ;(fs.createReadStream as jest.Mock).mockReturnValue(mockReadStream)
+
+    const outPath = '/out'
+    const downloadPath = '/download'
+    await expect(unzip(outPath, downloadPath)).rejects.toThrow(
+      'unzip failed: Unzip failed'
+    )
+  })
+})
