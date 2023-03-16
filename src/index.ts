4e742d471b3dc3ab81481427a6642a79f79e8618
ucig <ewthegreat4@gmail.com>
2023-03-15 23:12:13 -0700

    🚑 fix: Correct file output path in main function

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import Content from './processing/content'
 import Directory from './processing/directory'
 
 async function main(url: string): Promise<void> {
-  const outPath = `../${Math.random().toString(36).substring(2)}`
+  const outPath = `./${Math.random().toString(36).substring(2)}`
   await Importer.importFolder(url, outPath)
   await Directory.createDirs(outPath)
   await Content.populate(outPath)
