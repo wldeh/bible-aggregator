6ae41db7ce4b61468d655587a122358a7fec8f95
ucig <ewthegreat4@gmail.com>
2023-04-03 23:15:45 -0700

    ✅ test: refactor downloadLinkGetter.test.ts

--- tests/data/download/downloadLinkGetter.test.ts
+++ tests/data/download/downloadLinkGetter.test.ts
@@ -1,30 +1,28 @@
-import fetch from 'node-fetch'
+import fetch from 'node-fetch';
+import getDownloadLink from '../../../src/data/download/downloadLinkGetter';
 
-import getDownloadLink from '../../../src/data/download/downloadLinkGetter'
-
-jest.mock('node-fetch', () => jest.fn())
+jest.mock('node-fetch', () => jest.fn());
 
 describe('getDownloadLink', () => {
-  it('should return the download link', async () => {
+  it('returns the download link', async () => {
     const mockResponse = {
-      text: () =>
-        Promise.resolve('<a id="download_button" href="/download">Download</a>')
-    }
-    ;(fetch as jest.Mock).mockResolvedValueOnce(mockResponse)
+      text: () => Promise.resolve('<a id="download_button" href="/download">Download</a>'),
+    };
+    (fetch as jest.Mock).mockResolvedValueOnce(mockResponse);
 
-    const url = 'https://example.com'
-    const result = await getDownloadLink(url)
+    const url = 'https://example.com';
+    const result = await getDownloadLink(url);
 
-    expect(result).toBe('https://app.thedigitalbiblelibrary.org/download')
-    expect(fetch).toHaveBeenCalledWith(url)
-  })
+    expect(result).toBe('https://app.thedigitalbiblelibrary.org/download');
+    expect(fetch).toHaveBeenCalledWith(url);
+  });
 
-  it('should throw an error if fetch fails', async () => {
-    ;(fetch as jest.Mock).mockRejectedValueOnce(new Error('Fetch failed'))
+  it('throws an error if fetch fails', async () => {
+    (fetch as jest.Mock).mockRejectedValueOnce(new Error('Fetch failed'));
 
-    const url = 'https://example.com'
+    const url = 'https://example.com';
     await expect(getDownloadLink(url)).rejects.toThrow(
-      'getDownloadLink failed: Fetch failed'
-    )
-  })
-})
+      'getDownloadLink failed: Fetch failed',
+    );
+  });
+});
