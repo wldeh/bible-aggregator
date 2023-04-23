3fa1d4d243acc3245478610eab0d7f80be9575e1
ucig <ewthegreat4@gmail.com>
2023-04-22 22:44:11 -0700

    refactor: remove unused error class

--- src/errors/processingError.ts
+++ src/errors/processingError.ts
@@ -1,10 +1,1 @@
-/*export default class processingError extends Error {
-  code: string
-  method: string
 
-  constructor(message: string, code: string, method: string) {
-    super(message)
-    this.code = code
-    this.method = method
-  }
-}*/
