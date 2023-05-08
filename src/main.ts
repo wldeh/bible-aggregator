68550ed3c09a8aac8047a5c8d72ea99853b0e1ae
ucig <ewthegreat4@gmail.com>
2023-05-08 15:03:55 -0700

    feat: add timer and improve logging

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ export default async function main(): Promise<void> {
 
     for await (const item of array) {
       count++
-      console.log(`(${count / array.length}) Setting up: ${item[4]}`)
+      console.log(`(${count + '/' + array.length}) Setting up: ${item[4]}`)
       await setupBible(
         `https://app.thedigitalbiblelibrary.org/entry?id=${item[0]}`
       )
@@ -28,11 +28,16 @@ export default async function main(): Promise<void> {
 
 async function setupBible(url: string): Promise<void> {
   try {
+    const startTime = performance.now()
     const outPath = `./${Math.random().toString(36).substring(2)}`
 
     await Importer.importFolder(url, outPath)
     await Directory.createDirs(outPath)
     await Content.populate(outPath)
+
+    const endTime = performance.now()
+    const timeTaken = endTime - startTime
+    console.log(`Finished In ${(timeTaken / 1000).toFixed(2)} seconds @ ${new Date().toLocaleString('en-US', {timeZone: 'America/Los_Angeles'})}}`)
   } catch (error) {
     console.error(`Error setting up Bible: ${error.message}`)
   }
